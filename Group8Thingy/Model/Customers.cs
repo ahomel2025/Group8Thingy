53b78070f0a3db24c15f81041d8f1a925b67b063
Meghan Morsch <meghan.morsch@marquette.edu>
2023-11-30 15:14:02 -0600

    create customers class with authentication method

---
+++ Group8Thingy/Model/Customers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group8Thingy.Model;
+
+internal class Customers
+{
+    public List<Customer> customers {  get; set; }
+    public Customers() 
+    {
+        customers = new List<Customer>();
+    }
+
+    public Customers Authenticate(string username, string password)
+    {
+        var c = customers.Where(o => (o.UserName == username) && (o.Password == password));
+
+        if(c.Count() > 0 )
+        {
+            return c.First();
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+
+
+
+}
