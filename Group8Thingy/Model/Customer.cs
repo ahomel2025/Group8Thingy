e743d82ccf13f2586dd803249d58916e303cd1bd
Meghan Morsch <meghan.morsch@marquette.edu>
2023-11-30 14:47:36 -0600

    Create customer class

---
+++ Group8Thingy/Model/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group8Thingy.Model
+{
+    internal class Customer
+    {
+        public int Id { get; set; }
+        public string Lastname {  get; set; }
+        public string Firstname { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+    }
+}
