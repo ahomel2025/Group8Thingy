42e20715967ec2e85bca620a72b75288d9999100
Adam Homel <adam.d.homel@marquette.edu>
2023-11-30 14:48:14 -0600

    I made the class get information on the personal user

---
+++ Group8Thingy/Model/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group8Thingy.Model
+{
+    internal class Customer
+    {
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string password { get; set; }
+    }
+}
